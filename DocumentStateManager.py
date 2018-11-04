74b489a2efee9d80c4edadd0596c36a95be60632
andrey emelyanenko <sir.crusader@yandex.ru>
2018-11-05 01:03:51 +0300

    Update DocumentStateManager.py

--- DocumentStateManager.py
+++ DocumentStateManager.py
@@ -17,11 +17,9 @@ class DocumentStateManager(object):
         self.document = self.__get()
         if self.document != None:
             print("OK")
-            self.data = self.document['_source']['data']
-            self.id = self.document['_source']['key']
+            self.id = self.document['key']
         else:
             print("NOT OK")
-            self.data = None
             self.id = None
 
     def __get(self):
@@ -31,7 +29,7 @@ class DocumentStateManager(object):
             print(res)
             if res['hits']['total'] != 0:
                 print("Not None")
-                resparsed = res['hits']['hits'][0]
+                resparsed = res['hits']['hits'][0]['_source']
                 print(json.dumps(json.loads(json.dumps(resparsed)), indent=4, sort_keys=True))
                 return resparsed
             else:
@@ -50,9 +48,9 @@ class DocumentStateManager(object):
         """External method 'ChangeState' - deletes id in index in Elasticsearch and creates id in NewIndex."""
         try:
             print("ChangeState")
-            res = self.es.index(index=NewIndex, doc_type="all", id=self.id, body=self.document['_source'], refresh=True)
+            res = self.es.index(index=NewIndex, doc_type="all", id=self.id, body=self.document, refresh=True)
             print("delete")
-            self.es.delete(index=self.bufferindex, doc_type="all", id=self.id)
+            self.es.delete(index=self.bufferindex, doc_type="all", id=self.id, refresh=True)
             print(res)
             return res
         except exceptions as error:
@@ -71,12 +69,12 @@ class DocumentStateManager(object):
 if __name__ == "__main__":
     print(sys.version)
     DocumentStateManager = DocumentStateManager("http://172.26.7.84:9200/")
-    if DocumentStateManager.data:
-        print(DocumentStateManager.data)
-    if DocumentStateManager.id:
+    if DocumentStateManager.document != None:
+        print(DocumentStateManager.document)
+    if DocumentStateManager.id != None:
         print(DocumentStateManager.id)
 
-    if DocumentStateManager.data:
+    if DocumentStateManager.document != None:
         DocumentStateManager.ChangeState("testindex4")
         DocumentStateManager.Search("bufferindex")
         DocumentStateManager.Search("testindex4")
