bad570436444f42b5953e6a9d343823e10a86585
andrey emelyanenko <sir.crusader@yandex.ru>
2018-11-05 16:54:17 +0300

    Final

--- DocumentStateManager.py
+++ DocumentStateManager.py
@@ -1,5 +1,5 @@
-from elasticsearch import Elasticsearch
 from elasticsearch import exceptions
+import ElasticsearchCrawlerClient
 from elasticsearch.connection import create_ssl_context
 import time
 import datetime
@@ -10,28 +10,24 @@ import sys
 class DocumentStateManager(object):
     """DocumentStateManager is a CrawlerClient for Elasticsearch."""
 
-    def __init__(self, url):
+    def __init__(self, ElasticsearchCrawlerClient, BaseIndex="bufferindex"):
         """DocumentStateManager Constructor"""
-        self.es = Elasticsearch(url)
-        self.bufferindex = str("bufferindex")
-        self.document = self.__get()
+        self.base_index = BaseIndex
+        self.document = self.__get_document(ElasticsearchCrawlerClient, self.base_index)
         if self.document != None:
-            print("OK")
             self.id = self.document['key']
         else:
-            print("NOT OK")
             self.id = None
 
-    def __get(self):
+    def __get_document(self, ElasticsearchCrawlerClient, index):
         """Internal method '__get' - get id in index in Elasticsearch."""
         try:
-            res = self.es.search(index=self.bufferindex, doc_type="all", size=1)
-            print(res)
+            res = ElasticsearchCrawlerClient.search(index=index, doc_type="all", size=1)
             if res['hits']['total'] != 0:
                 print("Not None")
-                resparsed = res['hits']['hits'][0]['_source']
-                print(json.dumps(json.loads(json.dumps(resparsed)), indent=4, sort_keys=True))
-                return resparsed
+                parsed_result = res['hits']['hits'][0]['_source']
+                print(json.dumps(json.loads(json.dumps(parsed_result)), indent=4, sort_keys=True))
+                return parsed_result
             else:
                 print("None")
                 return None
@@ -44,37 +40,24 @@ class DocumentStateManager(object):
         except exceptions.__all__ as error:
             raise error
 
-    def ChangeState(self, NewIndex):
+    def change_state(self, ElasticsearchCrawlerClient, NewIndex):
         """External method 'ChangeState' - deletes id in index in Elasticsearch and creates id in NewIndex."""
         try:
             print("ChangeState")
-            res = self.es.index(index=NewIndex, doc_type="all", id=self.id, body=self.document, refresh=True)
-            print("delete")
-            self.es.delete(index=self.bufferindex, doc_type="all", id=self.id, refresh=True)
-            print(res)
-            return res
+            ElasticsearchCrawlerClient.buffer_put(index=NewIndex, doc_type="all", key=self.id, body=self.document)
+            print("DeleteInBuffer")
+            ElasticsearchCrawlerClient.delete_document(index=self.base_index, doc_type="all", id=self.id)
+            return True
         except exceptions as error:
             print(error)
             raise error
 
-    def Search(self, index):
-        """External method 'search' - searches data in Elasticsearch."""
-        print("Search")
-        res = self.es.search(index=index)
-        parsed = json.loads(json.dumps(res))
-        print(json.dumps(parsed, indent=4, sort_keys=True))
-        return res
-
 
 if __name__ == "__main__":
     print(sys.version)
-    DocumentStateManager = DocumentStateManager("http://172.26.7.84:9200/")
+
+    Client = ElasticsearchCrawlerClient.ElasticsearchCrawlerClient("http://172.26.7.84:9200/")
+    DocumentStateManager = DocumentStateManager(Client, "bufferindex")
     if DocumentStateManager.document != None:
         print(DocumentStateManager.document)
-    if DocumentStateManager.id != None:
-        print(DocumentStateManager.id)
-
-    if DocumentStateManager.document != None:
-        DocumentStateManager.ChangeState("testindex4")
-        DocumentStateManager.Search("bufferindex")
-        DocumentStateManager.Search("testindex4")
+        DocumentStateManager.change_state(Client, "testindex4")
--- elasticsearchCrawlerClient.py
+++ elasticsearchCrawlerClient.py
@@ -1,8 +1,10 @@
 from elasticsearch import Elasticsearch
 from elasticsearch import exceptions
 from elasticsearch.connection import create_ssl_context
+import time
 import datetime
 import json
+import sys
 
 
 class ElasticsearchCrawlerClient(object):
@@ -12,15 +14,17 @@ class ElasticsearchCrawlerClient(object):
         """ElasticsearchCrawlerClient Constructor"""
         print(url)
         self.es = Elasticsearch(url)
+        self.bufferindex = "bufferindex"
+        self.fulltextindex = "fulltext"
 
     def contains(self, key):
         """External method 'contains' - checks, if this key already exists in Elasticsearch. Uses '__contains'."""
-        self.__contains("fulltext", "all", key)
+        self.__contains(self.fulltextindex, "all", key)
 
-    def __contains(self, indexES, typeES, idES):
+    def __contains(self, index, doc_type, key):
         """Internal method '__contains' - checks, if this key already exists in Elasticsearch."""
         try:
-            res = self.es.get(index=indexES, doc_type=typeES, id=idES)
+            res = self.es.get(index=index, doc_type=doc_type, id=key)
             print(res)
             print(res['_source'])
             return True
@@ -31,34 +35,44 @@ class ElasticsearchCrawlerClient(object):
             else:
                 raise error
 
-    def put(self, key, data, date, head, tags, index="fulltext"):
+    def put(self, key, data, date, tags, index="fulltext"):
         """External method 'put' - puts data to Elasticsearch. Uses '__put'."""
         doc = {
             'key': key,
             'data': data,
             'date': date,
-			'head': head,
             'tags': tags,
             'timestamp': datetime.datetime.now()
         }
         print(doc)
         self.__put(index, "all", key, doc)
 
-    def __put(self, indexES, typeES, idES, dataES):
+    def __put(self, index, doc_type, key, body):
         """Internal method '__put' - puts data to Elasticsearch."""
         try:
-            res = self.es.index(index=indexES, doc_type=typeES, id=idES, body=dataES, refresh=True)
+            res = self.es.index(index=index, doc_type=doc_type, id=key, body=body, refresh=True)
+            self.buffer_put(index=self.bufferindex, doc_type=doc_type, key=key, body=body)
             print(res)
             return res
         except exceptions as error:
             print("ERROR:"+error)
             raise error
 
-    def search(self, index):
+    def buffer_put(self, index, doc_type, key, body):
+        """Internal method '__put' - puts data to Elasticsearch."""
+        try:
+            res = self.es.index(index=index, doc_type=doc_type, id=key, body=body, refresh=True)
+            print(res)
+            return res
+        except exceptions as error:
+            print("ERROR:" + error)
+            raise error
+
+    def search(self, index, doc_type="all", size=10):
         """External method 'search' - searches data in Elasticsearch."""
-        res = self.es.search(index=index)
+        res = self.es.search(index=index, doc_type=doc_type, size=size)
         # print(res['hits']['hits'])
-        parsed = json.loads(json.dumps(res['hits']['hits']))
+        parsed = json.loads(json.dumps(res))
         print(json.dumps(parsed, indent=4, sort_keys=True))
         return res
 
@@ -73,9 +87,17 @@ class ElasticsearchCrawlerClient(object):
             else:
                 raise error
 
-    def delete(self, index):
+    def delete_index(self, index):
         """External method 'delete' - deletes index in Elasticsearch."""
         self.es.indices.delete(index=index)
+
+    def delete_document(self, index, doc_type, id):
+        """External method 'delete' - deletes index in Elasticsearch."""
+        try:
+            self.es.delete(index=index, doc_type=doc_type, id=id, refresh=True)
+            print("Document is successfully deleted!")
+        except exceptions.RequestError as error:
+            raise error
 
     def exists(self, index):
         """External method 'exists' - exists index in Elasticsearch."""
@@ -88,14 +110,50 @@ class ElasticsearchCrawlerClient(object):
         print(res['_source'])
         return res
 
+    def scroll_search(self, index="_all"):
+        """External method 'search' - searches data in Elasticsearch.
+        https: // stackoverflow.com / questions / 46604207 / elasticsearch - scroll
+        """
+        res = self.es.search(
+            index=index,
+            doc_type='all',
+            scroll='10s',
+            size=100,
+            body={
+                # Your query's body
+            })
+        parsed = json.loads(json.dumps(res))
+        print(json.dumps(parsed, indent=4, sort_keys=True))
+        sid = res['_scroll_id']
+        print(sid)
+        print(res['hits']['total'])
+        return sid
+
+    def scroll(self, scroll_id):
+        while True:
+            time.sleep(5)
+            res = self.es.scroll(scroll_id=scroll_id)
+            print(json.dumps(json.loads(json.dumps(res)), indent=4, sort_keys=True))
+            self.put(key=datetime.datetime.now(), data="Some data here!", date="2018.11.02", tags=["tag0", "tag1", "tag2"], index="tag100500")
 
 if __name__ == "__main__":
+    print(sys.version)
     elasticsearchCrawlerClient = ElasticsearchCrawlerClient("http://172.26.7.84:9200/")
-    elasticsearchCrawlerClient.put(key="https://google.com/", data="Some data here!", date="2018.11.02", head="SuperHead", tags=["tag0", "tag1", "tag2"])
-    elasticsearchCrawlerClient.contains(key="https://google.com/")
-    #elasticsearchCrawlerClient.search("_all")
+    elasticsearchCrawlerClient.put(key="5", data="Some data here!", date="2018.11.02", tags=["tag0", "tag1", "tag2"], index="testindex3")
+    # elasticsearchCrawlerClient.contains(key="https://google.com/")
+    # elasticsearchCrawlerClient.scroll(elasticsearchCrawlerClient.scroll_search())
+    print("__________________________")
+    elasticsearchCrawlerClient.search("bufferindex")
+
+
     # elasticsearch.helpers.bulk(request, "index", stats_only=False)
-    # elasticsearchCrawlerClient.delete("testindex1")
-    # print(elasticsearchCrawlerClient.__dict__)
-    # print(elasticsearchCrawlerClient.__dict__.keys())
-    # print(elasticsearchCrawlerClient.__doc__)
+    # elastic.delete("testindex1")
+    # print(elastic.__dict__)
+    # print(elastic.__dict__.keys())
+    # print(elastic.__doc__)
+
+    # for hit in elasticsearchCrawlerClient.scrolled_search(index='_all', scroll='5m')['hits']['hits']:
+    #    print("---")
+    #    print(hit)
+
+    # elasticsearchCrawlerClient.("DnF1ZXJ5VGhlbkZldGNoFAAAAAAAAAITFmxLRDN5clEyU1FpUy1JZTJBdVJjMHcAAAAAAAACGBZsS0QzeXJRMlNRaVMtSWUyQXVSYzB3AAAAAAAAAhQWbEtEM3lyUTJTUWlTLUllMkF1UmMwdwAAAAAAAAIVFmxLRDN5clEyU1FpUy1JZTJBdVJjMHcAAAAAAAACGRZsS0QzeXJRMlNRaVMtSWUyQXVSYzB3AAAAAAAAAhYWbEtEM3lyUTJTUWlTLUllMkF1UmMwdwAAAAAAAAIXFmxLRDN5clEyU1FpUy1JZTJBdVJjMHcAAAAAAAACGhZsS0QzeXJRMlNRaVMtSWUyQXVSYzB3AAAAAAAAAhsWbEtEM3lyUTJTUWlTLUllMkF1UmMwdwAAAAAAAAIdFmxLRDN5clEyU1FpUy1JZTJBdVJjMHcAAAAAAAACHBZsS0QzeXJRMlNRaVMtSWUyQXVSYzB3AAAAAAAAAiYWbEtEM3lyUTJTUWlTLUllMkF1UmMwdwAAAAAAAAIeFmxLRDN5clEyU1FpUy1JZTJBdVJjMHcAAAAAAAACHxZsS0QzeXJRMlNRaVMtSWUyQXVSYzB3AAAAAAAAAiEWbEtEM3lyUTJTUWlTLUllMkF1UmMwdwAAAAAAAAIgFmxLRDN5clEyU1FpUy1JZTJBdVJjMHcAAAAAAAACIhZsS0QzeXJRMlNRaVMtSWUyQXVSYzB3AAAAAAAAAiMWbEtEM3lyUTJTUWlTLUllMkF1UmMwdwAAAAAAAAIkFmxLRDN5clEyU1FpUy1JZTJBdVJjMHcAAAAAAAACJRZsS0QzeXJRMlNRaVMtSWUyQXVSYzB3")
